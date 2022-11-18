2d91a4d1f29e5fe4cf1009fa9679e630e83c81b2
pranav-kural <pranavkural042@gmail.com>
2022-11-17 22:58:59 -0500

    completed initial implementation & testing of Tries Search

---
+++ src/TriesDataObj.java
@@ -0,0 +1,30 @@
+import java.util.List;
+
+/**
+ * Store a data object to be used in storing TriesSearch
+ */
+public class TriesDataObj {
+    private String key;
+    private List<String> words;
+
+    public TriesDataObj(String key, List<String> words) {
+        this.key = key;
+        this.words = words;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public List<String> getWords() {
+        return words;
+    }
+
+    public void setWords(List<String> words) {
+        this.words = words;
+    }
+}
